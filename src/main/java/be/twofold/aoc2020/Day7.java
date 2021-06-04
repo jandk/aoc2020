6638ec5e09cd2ad383908a366e39cd27f239f8df
Jan De Kock <jan@twofold.be>
2021-06-04 07:53:53 +0200

    Like this solution better

--- src/main/java/be/twofold/aoc2020/Day7.java
+++ src/main/java/be/twofold/aoc2020/Day7.java
@@ -1,83 +1,76 @@
 package be.twofold.aoc2020;
 
 import java.util.*;
-import java.util.stream.*;
+import java.util.regex.*;
 
 public class Day7 {
+    private static final Pattern FullPattern = Pattern.compile("([\\w ]+) bags contain (.+)");
+    private static final Pattern PartPattern = Pattern.compile("(\\d+) ([\\w ]+) bags?");
+
     public static void main(String[] args) {
         List<String> lines = Util.readResouce("/day7.txt");
 
-        List<Edge> edges = lines.stream()
-            .flatMap(line -> parseBag(line).stream())
-            .collect(Collectors.toUnmodifiableList());
+        Map<String, Map<String, Integer>> adj = new HashMap<>();
 
-        part1(edges);
-        part2(edges);
+        for (String line : lines) {
+            Matcher fullMatcher = FullPattern.matcher(line);
+            if (!fullMatcher.matches()) {
+                throw new UnsupportedOperationException();
+            }
+
+            String source = fullMatcher.group(1);
+            String content = fullMatcher.group(2);
+            Matcher partMatcher = PartPattern.matcher(content);
+            while (partMatcher.find()) {
+                int amount = Integer.parseInt(partMatcher.group(1));
+                String target = partMatcher.group(2);
+                adj
+                    .computeIfAbsent(source, __ -> new HashMap<>())
+                    .put(target, amount);
+            }
+        }
+
+        part1(adj);
+        part2(adj);
     }
 
-    private static void part1(List<Edge> edges) {
-        Map<String, Set<String>> adj = new HashMap<>();
-        edges.forEach(edge -> adj
-            .computeIfAbsent(edge.target, __ -> new HashSet<>())
-            .add(edge.source));
+    private static void part1(Map<String, Map<String, Integer>> adj) {
+        Map<String, Map<String, Integer>> rev = new HashMap<>();
+        for (Map.Entry<String, Map<String, Integer>> e1 : adj.entrySet()) {
+            for (Map.Entry<String, Integer> e2 : e1.getValue().entrySet()) {
+                String source = e1.getKey();
+                String target = e2.getKey();
+                int amount = e2.getValue();
+                rev
+                    .computeIfAbsent(target, __ -> new HashMap<>())
+                    .put(source, amount);
+            }
+        }
 
         Set<String> visited = new HashSet<>();
-        dfs(adj, visited, "shiny gold");
+        dfs(rev, visited, "shiny gold");
         System.out.println(visited.size() - 1);
     }
 
-    private static void dfs(Map<String, Set<String>> adj, Set<String> visited, String node) {
+    private static void dfs(Map<String, Map<String, Integer>> adj, Set<String> visited, String node) {
         visited.add(node);
-        for (String n : adj.getOrDefault(node, Set.of())) {
-            if (!visited.contains(n)) {
-                dfs(adj, visited, n);
+        for (String e : adj.getOrDefault(node, Map.of()).keySet()) {
+            if (!visited.contains(e)) {
+                dfs(adj, visited, e);
             }
         }
     }
 
-    private static void part2(List<Edge> edges) {
-        Map<String, List<Edge>> adj = edges.stream()
-            .collect(Collectors.groupingBy(edge -> edge.source));
-
-        Edge start = new Edge(null, "shiny gold", 1);
-
-        int count = countBags(adj, start);
+    private static void part2(Map<String, Map<String, Integer>> adj) {
+        int count = countBags(adj, "shiny gold", 1);
         System.out.println(count - 1);
     }
 
-    private static int countBags(Map<String, List<Edge>> adj, Edge edge) {
-        int sum = edge.amount;
-        for (Edge e : adj.getOrDefault(edge.target, List.of())) {
-            sum += countBags(adj, e) * edge.amount;
+    private static int countBags(Map<String, Map<String, Integer>> adj, String bag, int amount) {
+        int sum = amount;
+        for (Map.Entry<String, Integer> e : adj.getOrDefault(bag, Map.of()).entrySet()) {
+            sum += countBags(adj, e.getKey(), e.getValue()) * amount;
         }
         return sum;
-    }
-
-    private static List<Edge> parseBag(String line) {
-        int contain = line.indexOf(" bags contain ");
-        String color = line.substring(0, contain);
-
-        String[] content = line.substring(contain + " bags contain ".length()).split(", ");
-
-        return Arrays.stream(content)
-            .map(s -> s.substring(0, s.lastIndexOf(' ')))
-            .filter(s -> !s.equals("no other"))
-            .map(s -> new Edge(
-                color,
-                s.substring(s.indexOf(' ') + 1),
-                Integer.parseInt(s, 0, s.indexOf(' '), 10)))
-            .collect(Collectors.toUnmodifiableList());
-    }
-
-    private static final class Edge {
-        private final String source;
-        private final String target;
-        private final int amount;
-
-        private Edge(String source, String target, int amount) {
-            this.source = source;
-            this.target = target;
-            this.amount = amount;
-        }
     }
 }
